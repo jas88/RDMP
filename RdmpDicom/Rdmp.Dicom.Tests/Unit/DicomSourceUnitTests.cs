c2b754154987391a99b0f781eeb273e44812c45a
James A Sutherland <j@sutherland.pw>
2025-11-01 16:20:59 -0500

    Use relative paths within zip file list for DICOM workload

--- RdmpDicom/Rdmp.Dicom.Tests/Unit/DicomSourceUnitTests.cs
+++ RdmpDicom/Rdmp.Dicom.Tests/Unit/DicomSourceUnitTests.cs
@@ -90,7 +90,7 @@ public sealed class DicomSourceUnitTests
 
         var source = new DicomFileCollectionSource { FilenameField = "RelativeFileArchiveURI" };
         File.WriteAllLines(txt,
-            Directory.EnumerateFileSystemEntries(dir).Select(e => $"{zip}!{e}"));
+            Directory.EnumerateFileSystemEntries(dir).Select(e => $"{zip}!{Path.GetRelativePath(dir,e)}"));
         File.ReadAllLines(txt).Each(Console.Error.WriteLine);
         source.PreInitialize(new FlatFileToLoadDicomFileWorklist(new(new(txt))), ThrowImmediatelyDataLoadEventListener.Quiet);
         var toMemory = new ToMemoryDataLoadEventListener(true);
