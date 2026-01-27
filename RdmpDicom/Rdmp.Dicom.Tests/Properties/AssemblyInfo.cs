451e156ac61562252525a09325dd58dfb952ca75
James A Sutherland <j@sutherland.pw>
2026-01-26 21:54:03 -0600

    fix: disable parallel test execution to work around MTP/NUnit hang
    Add [assembly: NonParallelizable] to all test projects. This works
    around a known issue where NUnit + MTP hangs on shutdown when tests
    run in parallel. See: https://github.com/nunit/nunit3-vs-adapter/issues/1282

--- RdmpDicom/Rdmp.Dicom.Tests/Properties/AssemblyInfo.cs
+++ RdmpDicom/Rdmp.Dicom.Tests/Properties/AssemblyInfo.cs
@@ -1,24 +1,11 @@
-﻿// Copyright (c) The University of Dundee 2018-2025
+// Copyright (c) The University of Dundee 2018-2025
 // This file is part of the Research Data Management Platform (RDMP).
 // RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 // RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
-using System.Runtime.InteropServices;
-using NUnit.Framework;
 
-// Default timeout of 60 seconds per test to catch hanging tests
-#pragma warning disable CS0618 // Timeout is deprecated but CancelAfter doesn't support assembly level
-[assembly: Timeout(60000)]
-#pragma warning restore CS0618
+using NUnit.Framework;
 
 // Disable parallel test execution to work around MTP/NUnit hang on shutdown
 // See: https://github.com/nunit/nunit3-vs-adapter/issues/1282
 [assembly: NonParallelizable]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("13825638-5252-413c-98bc-1aef3b1cb9e4")]
